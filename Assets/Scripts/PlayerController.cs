a94f3f049e7caccc3e2bb91fdd80ebd600e3967b
swrczk <harleenincube@gmail.com>
2022-02-13 21:21:55 +0100

    double jump - bug fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,19 +15,19 @@ public class PlayerController : MonoBehaviour
 
     Animator animator;
     Rigidbody2D rgBody;
+    CapsuleCollider2D collider;
 
     LifesManager lifesManager;
     SceneManager sceneManager;
     bool dirRight = true;
-
-    bool onGround = true;
-    float radius = 0.1f;
+    float extraHeight = .01f;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rgBody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CapsuleCollider2D>();
         lifesManager = GameObject.Find("Manager").GetComponent<LifesManager>();
         sceneManager = GameObject.Find("Manager").GetComponent<SceneManager>();
     }
@@ -36,35 +36,50 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (rgBody == null) return;
+
+        HorizontalMove();
+        VerticalMove();
+    }
+
+    void HorizontalMove()
+    {
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("DogDeadBush"))
         {
             rgBody.velocity = Vector2.zero;
             return;
         }
-
-        onGround = Physics2D.OverlapCircle(GroundTester.position, radius, GroundLayerMask);
-
         float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
         animator.SetFloat("speed", Math.Abs(horizontalMove));
-
-
         rgBody.velocity = new Vector2(horizontalMove * HeroSpeed, rgBody.velocity.y);
-        if (horizontalMove < 0 && dirRight)
-        {
-            Flip();
-        }
-        else if (horizontalMove > 0 && !dirRight)
-        {
-            Flip();
-        }
 
-        bool heroJump = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) ||
-                        Input.GetKeyDown(KeyCode.W);
-        if (heroJump && onGround)
+        var turnLeft = horizontalMove < 0 && dirRight;
+        var turnRight = horizontalMove > 0 && !dirRight;
+        if (turnLeft || turnRight)
+        {
+            Flip();
+        }
+    }
+
+    void VerticalMove()
+    {
+        if (IsJumping() && IsOnGround())
         {
             Jump();
         }
+    }
+
+    bool IsOnGround()
+    {
+        RaycastHit2D raycast = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, extraHeight, GroundLayerMask);
+        // Debug.DrawRay(collider.bounds.center, Vector2.down * (collider.bounds.extents.y + extraHeight), raycast.collider != null ? Color.green : Color.red);
+        return raycast.collider != null;
+    }
+
+    bool IsJumping()
+    {
+        float verticalMove = Input.GetAxis("Vertical");
+        return Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) ||
+                        Input.GetKeyDown(KeyCode.W);
     }
 
     void Flip()
